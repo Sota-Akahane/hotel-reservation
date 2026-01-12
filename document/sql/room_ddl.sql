220bbc2eeed840186051af77647b8f376e234475
Sota-Akahane <sota258@docomo.ne.jp>
2026-01-12 17:36:29 +0900

    - 画像パスを含めるためにroomsテーブルのDDL,DMLを修正 - ホテル詳細画面のHTMLをthymeleaf用に修正

--- document/sql/room_ddl.sql
+++ document/sql/room_ddl.sql
@@ -6,5 +6,6 @@ CREATE TABLE rooms (
     capacity INTEGER NOT NULL,
     price INTEGER NOT NULL, -- 今は固定
     description TEXT,
+    image_path VARCHAR(255),
     FOREIGN KEY (hotel_id) REFERENCES hotels (id)
 );
